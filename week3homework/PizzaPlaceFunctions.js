2e3f4e9d28bf64b6e998246047a1e4368473d751
jrodrz <rekoshrodriguez81@gmail.com>
2023-04-13 17:23:03 -0500

    final pizza place functions 3.1.3 week 3 homework

--- week3homework/PizzaPlaceFunctions.js
+++ week3homework/PizzaPlaceFunctions.js
@@ -1,27 +1,26 @@
 const pizzaToppings = ["mushrooms", "pepperoni", "ham", "pineapple"];
 
-function greetCustomer(){
-  console.log(`Welcome to Pizza Jewel. This evening our toppings are ${toppings.map (item => item)}`);
+function greetCustomer() {
+  console.log(`Welcome to Pizza Jewel. This evening our toppings are ${toppings.map (item => item)}`)
 };
 
 function getPizzaOrder(size, crust, ...toppings){
-  console.log(`One large thick crust pizza with ${toppings.map (item => item)} coming up!`);
+  console.log(`One ${size}, ${crust} crust pizza with ${toppings.map (item => item)} coming up!`);
   return[size, crust, toppings];
-}
+};
 
-function preparePizza([size, crust, toppings]) {
+function preparePizza([size, crust, ...toppings]) {
   console.log("Cooking pizza.");
-  return {
-    size: size,
-    crust: crust,
-    toppings: toppings,
-  }
-}
+  return {size: any,
+         crust: any,
+         toppings: any}
+};
 
 function servePizza(pizzaObj){
-  console.log(`Order up! Your largethick crust pizza with ${pizzaObj.toppings} Enjoy!`)
+  console.log(`Order up! Enjoy your large thick crust pizza with ${pizzaObj.toppings}`)
+  return (pizzaObj);
 };
 
 getPizzaOrder();
-preparePizza();
-servePizza()
+preparePizza(getPizzaOrder);
+servePizza(preparePizza);
